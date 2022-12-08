bded8335344c03afd9a030e76a0c7c99718f9d2a
hoya <38947736+leehh312@icloud.com>
2022-12-08 21:33:02 +0900

    쇠막대기 알고리즘

---
+++ BaekJoon/Stack/steelstick/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var (
+	stack []rune = make([]rune, 0)
+	size  int    = 0
+)
+
+func Push(param rune) {
+	size++
+	stack = append(stack, param)
+}
+
+func Pop() rune {
+	result := stack[size-1]
+	size--
+	stack = stack[:size]
+
+	return result
+}
+
+/*
+()(((()())(())()))(()) = 17
+(((()(()()))(())()))(()()) = 24
+*/
+func main() {
+	w := bufio.NewWriter(os.Stdout)
+	defer w.Flush()
+
+	sc := bufio.NewScanner(os.Stdin)
+	sc.Buffer(make([]byte, 100001), 100001)
+	sc.Scan()
+	text := sc.Text()
+
+	var stick int = 0
+	var prevWord rune
+	for _, word := range text {
+		if word == '(' {
+			Push(word)
+		} else {
+			Pop()
+			// "(" 다음 ")"가 바로 들어오면 레이저
+			if prevWord == '(' {
+				stick += size // 레이저인 경우 스택 안의 괄호 갯수만큼 막대 카운트.
+			} else {
+				// ")" 다음 ")"가 들어온 경우 막대기 끝의 개수 하나 카운트.
+				stick++
+			}
+		}
+		prevWord = word
+	}
+
+	fmt.Fprintln(w, stick)
+}
